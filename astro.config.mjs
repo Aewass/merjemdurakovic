6038e7846a9f9b0a862818f169e5b30866e31930
Mirza@Evoila <mmuric@evoila.com>
2025-06-29 11:17:24 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  build: {
+    inlineStylesheets: "auto",
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            swiper: ["swiper"],
+          },
+        },
+      },
+    },
+  },
 });
